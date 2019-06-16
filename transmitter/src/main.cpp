271d87cc0de817c1c3ffc65647755e9dbc656d48
Antonio Vanegas <hpsaturn@gmail.com>
2019-06-16 18:05:18 +0200

    added basic transmitter code on specific directory project for it

---
+++ transmitter/src/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+#include <VirtualWire.h>
+
+void send(char *message)
+{
+  vw_send((uint8_t *)message, strlen(message));
+  vw_wait_tx(); // Wait until the whole message is gone
+}
+
+void setup()
+{
+  // Initialize the IO and ISR
+  vw_setup(2000); // Bits per sec
+}
+
+void loop()
+{
+  send("Hello there");
+  delay(1000);
+}
+
