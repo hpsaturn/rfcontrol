8ddb6ef49a6b8bc42c31ea812d3f07bd16b9c737
Antonio Vanegas <hpsaturn@gmail.com>
2019-07-02 08:47:45 +0200

    added joystick logic for train motor

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -25,9 +25,6 @@ struct Data_Package {
   uint8_t j1PotX;
   uint8_t j1PotY;
   uint8_t j1Button;
-  uint8_t j2PotX;
-  uint8_t j2PotY;
-  uint8_t j2Button;
   uint8_t tSwitch1;
   uint8_t tSwitch2;
   uint8_t buttonUp;
@@ -42,10 +39,7 @@ void resetData() {
   // Reset the values when there is no radio connection - Set initial default values
   data.j1PotX = 127;
   data.j1PotY = 127;
-  data.j2PotX = 127;
-  data.j2PotY = 127;
   data.j1Button = 1;
-  data.j2Button = 1;
   data.tSwitch1 = 1;
   data.tSwitch2 = 1;
   data.buttonUp = 1;
@@ -55,18 +49,14 @@ void resetData() {
 }
 
 void printData() {
-  Serial.print(" Up:");
-  Serial.print(data.buttonUp);
-  Serial.print(" Dn:");
-  Serial.print(data.buttonDn);
-  Serial.print(" Lf:");
-  Serial.print(data.buttonLf);
-  Serial.print(" Rg:");
-  Serial.print(data.buttonRg);
-  Serial.print(" MV:");
-  Serial.print(motor_val);
-  Serial.print(" MD:");
-  Serial.println(motor_dir);
+  Serial.print(" Jx:"); Serial.print(data.j1PotX);
+  Serial.print(" Jy:"); Serial.print(data.j1PotY);
+  Serial.print(" Up:"); Serial.print(data.buttonUp);
+  Serial.print(" Dn:"); Serial.print(data.buttonDn);
+  Serial.print(" Lf:"); Serial.print(data.buttonLf);
+  Serial.print(" Rg:"); Serial.print(data.buttonRg);
+  Serial.print(" MV:"); Serial.print(motor_val);
+  Serial.print(" MD:"); Serial.println(motor_dir);
 }
 
 void musicCheck() {
@@ -122,7 +112,34 @@ void loop() {
     if (data.buttonDn == 0 ) speedDn();
     if (data.buttonRg == 0 ) motor_dir = 0;
     if (data.buttonLf == 0 ) motor_dir = 1;
-    if (data.buttonLf == 0 && data.buttonRg == 0)toogleMusic = !toogleMusic;
+    if (data.buttonLf == 0 && data.buttonRg == 0) toogleMusic = !toogleMusic;
+
+    int xAxis = data.j1PotX;
+    int yAxis = data.j1PotY;
+    int motorSpeed = 0;
+
+    // Y-axis used for increasing or decreasing motor value control
+    if (yAxis > 140) {
+      // Convert the increasing Y-axis readings from 140 to 255 into 0 to 255 value for the PWM signal
+      motorSpeed = map(yAxis, 140, 255, 0, 255);
+      if(motorSpeed > motor_val) motor_val = motorSpeed;
+    }
+    else if (yAxis < 110) {
+      // Convert the declining Y-axis readings from 110 to 0 into 0 to 255 value for the PWM signal 
+      motorSpeed = map(yAxis, 110, 0, 255, 0);
+      if(motorSpeed < motor_val) motor_val = motorSpeed;
+    }
+    // X-axis used for backward and forward control
+    if (xAxis < 110) {
+      motorSpeed = map(xAxis, 110, 0, 255, 0);
+      if(motorSpeed < motor_val) motor_val = motorSpeed;
+      motor_dir = 1;
+    }
+    if (xAxis > 140) {
+      motorSpeed = map(xAxis, 140, 255, 0, 255);
+      if(motorSpeed > motor_val) motor_val = motorSpeed;
+      motor_dir = 0;
+    }
     // Serial.print("dlen:"); Serial.print(data_len);
     // printData();
   }
